f37e3d8bb08d12fbda3a3b5c7b24c3f110d1ab1d
sing3demons <sing3demons@live.com>
2022-03-03 18:18:13 +0700

    feat: gracefully shutdown, path x

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/sing3demons/todo/auth"
 	"github.com/sing3demons/todo/todo"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+)
+
+func init() {
+	err := godotenv.Load("dev.env")
+	if err != nil {
+		log.Printf("please consider environment variables: %s", err)
+	}
+}
+
+var (
+	buildcommit = "dev"
+	buildtime   = time.Now().String()
 )
 
 func main() {
@@ -17,16 +39,49 @@ func main() {
 	db.AutoMigrate(&todo.Todo{})
 
 	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
+	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
-			"message": "pong",
+			"message": "hello world",
 		})
 	})
 
-	r.GET("tokenz", auth.AccessToken("==signature=="))
-	protect := r.Group("", auth.Protect([]byte("==signature==")))
+	r.GET("x", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"buildcommit": buildcommit,
+			"buildtime":   buildtime,
+		})
+	})
+
+	r.GET("tokenz", auth.AccessToken(os.Getenv("SIGN")))
+	protect := r.Group("", auth.Protect([]byte(os.Getenv("SIGN"))))
 
 	handler := todo.NewTodoHandler(db)
 	protect.POST("/todos", handler.NewTask)
-	r.Run(":8080")
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	serve := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := serve.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := serve.Shutdown(timeoutCtx); err != nil {
+		fmt.Println(err)
+	}
 }
