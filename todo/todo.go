b617147f1a1b50ef14b4c00c00d43f814c4e5319
sing3demons <sing3demons@live.com>
2022-03-03 15:41:27 +0700

    feat: todo/

---
+++ todo/todo.go
@@ -0,0 +1,46 @@
+package todo
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	gorm.Model
+	Title string `json:"text"`
+}
+
+func (Todo) Tablename() string {
+	return "todos"
+}
+
+type TodoHandler struct {
+	db *gorm.DB
+}
+
+func NewTodoHandler(db *gorm.DB) *TodoHandler {
+	return &TodoHandler{db: db}
+}
+
+func (t *TodoHandler) NewTask(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := t.db.Create(&todo).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"ID": todo.Model.ID,
+	})
+}
